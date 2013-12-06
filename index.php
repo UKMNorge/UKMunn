a89ab226673220397742667de757448866f16253
Marius Mandal <mariusmandal@gmail.com>
2013-12-06 09:47:23 +0100

    Minify

--- index.php
+++ index.php
@@ -22,56 +22,70 @@ $url->send 		= $url->base.'send/';
 $url->sent		= $url->base.'sendt/';
 $url->print		= $url->base.'print/';
 
-
 $TWIG = array('url' => $url);
 
-$app->get('/', function() use($app, $TWIG) {
-	require_once('controllers/invite.controller.php');	
-	return $app['twig']->render( 'invite.twig.html', $TWIG );
-}); 
+/* ****************** ROUTING ****************** */
+	$app->get('/less/{filename}', function($filename) use($app, $TWIG) {
+		header("Content-Type: text/css");
+		header("X-Content-Type-Options: nosniff");
+		$filename = 'resources/'. $filename;
+		/* ****************** LESS CSS ****************** */
+		$less = new lessc;
+		try {
+			echo $less->compileFile($filename);
+			die();
+		} catch (exception $e) {
+			return "Fatal error: " . $e->getMessage();
+		}
+	});
 
-$app->post('/', function() use($app, $TWIG) {
-
-	if( sizeof( $_POST ) == 0 )
-        return $app->redirect('/');
-
-	require_once('controllers/create.controller.php');	
-	$HTML = $app['twig']->render( 'send.twig.html', $TWIG );
-	$HTML .= HANDLEBARS( dirname(__FILE__) );
-	return $HTML;
-}); 
-
-
-
-
-$app->post('/send/', function() use($app, $TWIG) {
-	require_once('controllers/send.controller.php');
-	if(!$ERROR)
-		return $app->redirect('/sendt/'.$ID.'-'.$PASS.'/');
-	return $app->redirect('/ikke_sendt/'. $ID.'-'.$PASS.'/');
-});
-
-$app->get('/sendt/{ID}-{PASS}/', function($ID, $PASS) use($app, $TWIG) {
-	require_once('controllers/card.controller.php');	
-	return $app['twig']->render( 'sent.twig.html', $TWIG );
-});
-$app->get('/ikke_sendt/{ID}-{PASS}/', function($ID, $PASS) use($app, $TWIG) {
-	require_once('controllers/card.controller.php');
-	$TWIG['error'] = $_SESSION['error'];
-	return $app['twig']->render( 'not_sent.twig.html', $TWIG );
-});
-
-
-
-$app->get('/{ID}-{PASS}/', function($ID, $PASS) use($app, $TWIG) {
-	require_once('controllers/card.controller.php');	
-	return $app['twig']->render( 'recipient.twig.html', $TWIG );
-}); 
-$app->get('/print/{ID}-{PASS}/', function($ID, $PASS) use($app, $TWIG) {
-	require_once('controllers/card.controller.php');	
-	return $app['twig']->render( 'printcard.twig.html', $TWIG );
-}); 
-
+	$app->get('/', function() use($app, $TWIG) {
+		require_once('controllers/invite.controller.php');	
+		return $app['twig']->render( 'invite.twig.html', $TWIG );
+	}); 
+	
+	$app->post('/', function() use($app, $TWIG) {
+	
+		if( sizeof( $_POST ) == 0 )
+	        return $app->redirect('/');
+	
+		require_once('controllers/create.controller.php');	
+		$HTML = $app['twig']->render( 'send.twig.html', $TWIG );
+		$HTML .= HANDLEBARS( dirname(__FILE__) );
+		return $HTML;
+	}); 
+	
+	
+	
+	
+	$app->post('/send/', function() use($app, $TWIG) {
+		require_once('controllers/send.controller.php');
+		if(!$ERROR)
+			return $app->redirect('/sendt/'.$ID.'-'.$PASS.'/');
+		return $app->redirect('/ikke_sendt/'. $ID.'-'.$PASS.'/');
+	});
+	
+	$app->get('/sendt/{ID}-{PASS}/', function($ID, $PASS) use($app, $TWIG) {
+		require_once('controllers/card.controller.php');	
+		return $app['twig']->render( 'sent.twig.html', $TWIG );
+	});
+	$app->get('/ikke_sendt/{ID}-{PASS}/', function($ID, $PASS) use($app, $TWIG) {
+		require_once('controllers/card.controller.php');
+		$TWIG['error'] = $_SESSION['error'];
+		return $app['twig']->render( 'not_sent.twig.html', $TWIG );
+	});
+	
+	
+	
+	$app->get('/{ID}-{PASS}/', function($ID, $PASS) use($app, $TWIG) {
+		require_once('controllers/card.controller.php');	
+		return $app['twig']->render( 'recipient.twig.html', $TWIG );
+	}); 
+	$app->get('/print/{ID}-{PASS}/', function($ID, $PASS) use($app, $TWIG) {
+		require_once('controllers/card.controller.php');	
+		return $app['twig']->render( 'printcard.twig.html', $TWIG );
+	}); 
+	
 
 $app->run();
 ?>
